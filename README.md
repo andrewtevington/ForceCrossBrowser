b587aa2b53c41d70c028ca7b0e90f375d60994dc
Andrew Tevington <atevington@workwave.com>
2017-02-14 10:11:44 -0500

    tweak utility

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 ﻿# ForceCrossBrowser
 
-Query Rally each release cycle to see which customers should be forced to cross browser.
+Query Rally to see which customers should be forced to cross browser.
 
 ## Usage
 
 The general syntax is:
 
-`node index.js feature release`
+`node index.js feature schedule state`
 
 For example:
 
-`node index.js F934 2017.50`
+`node index.js F933 In-Progress`
 
-This will query all accepted user stories under the `F934` feature with a release of `2017.50`. It will then pull the company key out of the case title, and turn on force cross browser for that company in the Squid site.
+This will query all in-progress user stories under the `F933` feature. It will then pull the company key out of the case title, and turn on force cross browser for that company in the Squid site.
 
 ## Authentication
 
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function getFeature(rallyApi, featureId, callback) {
 	rallyApi.query({
 		type: "portfolioitem",
 		limit: 1,
-		fetch: ["Project"],
 		query: queryUtils.where("FormattedID", "=", featureId)
 	}, function(error, response) {
 		if (error) {
@@ -13,26 +12,12 @@ function getFeature(rallyApi, featureId, callback) {
 	});
 }
 
-function getRelease(rallyApi, releaseName, project, callback) {
-	rallyApi.query({
-		type: "release",
-		limit: 1,
-		query: queryUtils.where("Name", "=", releaseName).and("Project", "=", project)
-	}, function(error, response) {
-		if (error) {
-			console.log(error);
-		} else if (response.Results.length) {
-			callback(response.Results[0]);
-		}
-	});
-}
-
-function getAcceptedUserStories(rallyApi, release, project, callback) {
+function getUserStories(rallyApi, portfolioItem, scheduleState, callback) {
 	rallyApi.query({
 		type: "hierarchicalrequirement",
 		limit: 1000,
 		fetch: ["Name"],
-		query: queryUtils.where("Release", "=", release).and("Project", "=", project).and("ScheduleState", "=", "Accepted")
+		query: queryUtils.where("PortfolioItem", "=", portfolioItem).and("ScheduleState", "=", scheduleState)
 	}, function(error, response) {
 		if (error) {
 			console.log(error);
@@ -77,6 +62,7 @@ function forceCompany(dynamoInstance, companyKey) {
 			
 			console.log(companyKey + " old url: " + oldUrl);
 			console.log(companyKey + " new url: " + newUrl);
+			console.log("");
 			
 			dynamoInstance.putItem(updateData, function(writeError) {
 				if (writeError) {
@@ -94,15 +80,13 @@ var queryUtils = rally.util.query;
 var aws = require("aws-sdk");
 var dynamoInstance = new aws.DynamoDB(auth.aws);
 var featureId = (process.argv[2] || "").trim();
-var releaseName = (process.argv[3] || "").trim();
+var scheduleState = (process.argv[3] || "").trim();
 
-if (featureId && releaseName) {
+if (featureId && scheduleState) {
 	getFeature(rallyApi, featureId, function(feature) {
-		getRelease(rallyApi, releaseName, feature.Project, function(release) {
-			getAcceptedUserStories(rallyApi, release, feature.Project, function(userStories) {
-				userStories.map(function(userStory) {
-					forceCompany(dynamoInstance, parseCompanyKey(userStory));
-				});
+		getUserStories(rallyApi, feature, scheduleState, function(userStories) {
+			userStories.map(function(userStory) {
+				forceCompany(dynamoInstance, parseCompanyKey(userStory));
 			});
 		});
 	});
